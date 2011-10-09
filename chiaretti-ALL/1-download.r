b5edfcab395182303f4dbde79fa1ca23f6d6bb9f
John Ramey <johnramey@gmail.com>
2011-10-09 15:11:34 -0700

    Updated the Chiaretti et al. (1999) data set's load, clean, and save process.

--- chiaretti-ALL/1-download.r
+++ chiaretti-ALL/1-download.r
@@ -1,7 +1,4 @@
 # A number of cancer data sets are on Bioconductor (http://www.bioconductor.org)
 # First, we install ALL of Bioconductor's R packages.
-source('http://bioconductor.org/biocLite.R')
-biocLite()
-
-# Downloading the Chiaretti Acute Lymphoblastic Leukemia (ALL) Data Set
+source("http://www.bioconductor.org/biocLite.R")
 biocLite('ALL')
